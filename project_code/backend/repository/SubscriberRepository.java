5b452b41fd393bddde9bbe6e51271d952be88e92
JackHat1 <panadreas5@gmail.com>
2025-03-07 14:06:03 +0200

    new

--- project_code/backend/repository/SubscriberRepository.java
+++ project_code/backend/repository/SubscriberRepository.java
@@ -5,19 +5,19 @@ public class SubscriberRepository {
     private Subscriber[] subscribers;
     private int count = 0;
 
-    private int getMAX_SUBSCRIBERS() {
+    public int getMAX_SUBSCRIBERS() {
         return MAX_SUBSCRIBERS;
     }
     // private void setMAX_SUBSCRIBERS(int mAX_SUBSCRIBERS) {
     //     MAX_SUBSCRIBERS = mAX_SUBSCRIBERS;
     // }
-    private Subscriber[] getSubscribers() {
+    protected Subscriber[] getSubscribers() {
         return subscribers;
     }
     // private void setSubscribers(Subscriber[] subscribers) {
     //     this.subscribers = subscribers;
     // }
-    private int getCount() {
+    public int getCount() {
         return count;
     }
     // private void setCount(int count) {
