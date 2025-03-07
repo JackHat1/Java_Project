11ad223ed4f1226c616dac2409638429120a20a6
JackHat1 <panadreas5@gmail.com>
2025-03-07 20:16:14 +0200

    new

--- project_code/backend/subscription/Subscriber.java
+++ project_code/backend/subscription/Subscriber.java
@@ -1,4 +1,7 @@
 package backend.subscription;
+
+import backend.content.Content; 
+
    public class Subscriber {
    private String subscriberEmail;
    private String subscriberPassword;
@@ -13,15 +16,10 @@ package backend.subscription;
    public String getSubscriberEmail() {
       return subscriberEmail;
    }
-   private void setSubscriberEmail(String subscriberEmail) {
-      this.subscriberEmail = subscriberEmail;
-   }
    // private String getSubscriberPassword() {
    //    return subscriberPassword;
    // }
-   private void setSubscriberPassword(String subscriberPassword) {
-      this.subscriberPassword = subscriberPassword;
-   }
+
    public SubscriptionPlan getPlan() {
       return plan;
    }
@@ -58,10 +56,51 @@ package backend.subscription;
    // private void setFavoriteCount(int favoriteCount) {
    //    this.favoriteCount = favoriteCount;
    // }
+   
    public Subscriber(String email, String password, SubscriptionPlan plan) {
       this.subscriberEmail = email;
       this.subscriberPassword = password;
       this.plan = plan;
   }
+
+   public void setSubscriberEmail(String email) {
+      if (email != null) { // Check if email is not null
+         boolean isValid = email.matches("^[A-Za-z0-9+_.-]+@[A-Za-z0-9.-]+$"); // Validate format
+         
+         if (isValid) { 
+            this.subscriberEmail = email;
+         }
+      }
+   }
+
+   public void setSubscriberPassword(String password) {
+         if (password != null && password.matches("^(?=.*[A-Z])(?=.*[a-z])(?=.*\\d)(?=.*[@#$%^&+=!]).{8,}$")) {
+            subscriberPassword = password; 
+         }
+   }
+
+   public void watchContent(Content content) {
+      int count = 0;
+      while (count < 100) { 
+         if (watchHistory[count] == null) {
+            watchHistory[count] = content; 
+            watchCount++; 
+            break; 
+         }
+         count++;
+      }
+   }
+
+   public void printHistory() {
+      for (int i = 0; i < watchCount; i++) {  
+          if (watchHistory[i] != null) {  
+              System.out.println(watchHistory[i].getTitle());  
+          }
+      }
+  }
+  
+
+  
+
   
 }
